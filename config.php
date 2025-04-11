ed2fde8b8a7f72ffcea06ed2dd21cfdfc0eb7ec2
bilal el kibir <b.elkibir@outlook.com>
2025-04-11 12:40:21 +0200

    x

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $user = "bit_academy";
 $password = "Jarvis123@";
 $database = "budgeteer_db";
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+$conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
